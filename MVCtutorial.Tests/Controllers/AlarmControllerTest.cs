3ff42c17bb56fd684ef8cf10281abd998beef9bd
Marek Novak <chodec.marek@gmail.com>
2016-05-17 12:54:03 +0200

    All codes excluding tests

---
+++ MVCtutorial.Tests/Controllers/AlarmControllerTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MVCtutorial;
+using MVCtutorial.Controllers;
+
+namespace MVCtutorial.Tests.Controllers
+{
+    class AlarmControllerTest
+    {
+
+
+        [TestClass]
+        public class HomeControllerTest
+        {
+            [TestMethod]
+            public void Form()
+            {
+                // Arrange
+                AlarmController controller = new AlarmController();
+
+                // Act
+                ViewResult result = controller.Form() as ViewResult;
+
+                // Assert
+                Assert.IsNotNull(result);
+            }
+        }
+    }
+}
