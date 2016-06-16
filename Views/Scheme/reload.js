b39e7cb401aac905a42b2b49a60eab4ed4c19818
Marek Novak <chodec.marek@gmail.com>
2016-06-16 13:40:57 +0200

    changes update Changes: scheme autoreload added

---
+++ Views/Scheme/reload.js
@@ -0,0 +1,14 @@
+﻿var t;
+
+function autoreload() {
+    loadImg();
+    t = setTimeout("autoreload", 20000); // perioda v ms
+}
+
+function loadImg() {
+    _im = $("<img>");
+
+    //_im.bind("load", function () { $(this).show(); $('div#schema').html(_im); } );
+    _im.attr('src', "/scheme/getImage");
+    _im.attr('alt', "schema");
+}
